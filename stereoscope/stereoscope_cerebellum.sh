e684d719b297ed7a5a16f0ed414ddc7a7e8a5da7
Xi He <43379286+xxihe@users.noreply.github.com>
2024-06-09 14:21:56 -0400

    Update stereoscope_cerebellum.sh

--- stereoscope/stereoscope_cerebellum.sh
+++ stereoscope/stereoscope_cerebellum.sh
@@ -2,5 +2,5 @@
 
 source activate stereoscope
 
-/usr/bin/time -v stereoscope run --sc_cnt /storage/group/qul12/default/retrofit/Mouse/CerebellumPuck_sc_count.tsv --sc_labels /storage/group/qul12/default/retrofit/Mouse/CerebellumPuck_sc_label.tsv \
---st_cnt /storage/group/qul12/default/retrofit/Mouse/CerebellumPuck_counts.tsv -o mouse_res_record --gpu -lr 0.01
+/usr/bin/time -v stereoscope run --sc_cnt CerebellumPuck_sc_count.tsv --sc_labels CerebellumPuck_sc_label.tsv \
+--st_cnt CerebellumPuck_counts.tsv -o mouse_res_record --gpu -lr 0.01
