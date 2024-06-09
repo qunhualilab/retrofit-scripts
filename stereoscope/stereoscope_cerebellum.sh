d68726cefa40b7c263d80c7b01a863c5a9b5849c
Xi He <43379286+xxihe@users.noreply.github.com>
2024-06-09 15:27:56 -0400

    Update stereoscope_cerebellum.sh

--- stereoscope/stereoscope_cerebellum.sh
+++ stereoscope/stereoscope_cerebellum.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 source activate stereoscope
-
-/usr/bin/time -v stereoscope run --sc_cnt CerebellumPuck_sc_count.tsv --sc_labels CerebellumPuck_sc_label.tsv \
+#
+stereoscope run --sc_cnt CerebellumPuck_sc_count.tsv --sc_labels CerebellumPuck_sc_label.tsv \
 --st_cnt CerebellumPuck_counts.tsv -o mouse_res_record --gpu -lr 0.01
+
+stereoscope run --sc_cnt Cerebellum_sc_count_K=10.tsv --sc_labels Cerebellum_sc_label_K=10.tsv \
+--st_cnt N=10,M=3_loc_X.tsv -o N=10,M=3_res --gpu -lr 0.01
