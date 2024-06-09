317c06d7b3c2f0af6dc6e649f8b8c3da40a44dd7
Xi He <43379286+xxihe@users.noreply.github.com>
2024-06-09 16:40:16 -0400

    Create CARDfree_colon.R

---
+++ cardfree/CARDfree_colon.R
@@ -0,0 +1,36 @@
+library(CARD)
+library(retrofit)
+library(DescTools)
+library(corrplot)
+##load ST and location files
+loc <- "A3_coords.csv"
+location <- read.csv(file = loc,row.names = 1)
+colnames(location) <- c("x", "y")
+
+input_x = read.csv("A3_counts.csv")
+rownames(input_x) = input_x[,1]
+input_x = as.matrix(input_x[,-1])
+# Create marker gene list: od gene + sc expression
+gt_W0 = read.csv("Intestine_W_12pcw.csv")
+rownames(gt_W0) = gt_W0[,1]
+gt_W0 = as.matrix(gt_W0[,-1])
+de_gene = read.table("od_gene_ColonA3.txt")
+sub_W = gt_W0[rownames(gt_W0) %in% de_gene_m,]
+cell_type = apply(sub_W, 1, which.max)
+
+mk_gene = list()
+for(i in 1:ncol(gt_W0)){
+  print(colnames(gt_W0)[i])
+  mk_gene = c(mk_gene, list(names(which(cell_type == i))))
+}
+names(mk_gene) = colnames(gt_W0)
+
+CARDfree_obj = createCARDfreeObject(markerList = mk_gene, 
+                                    spatial_count = input_x,  
+                                    spatial_location = location,
+                                    minCountGene = 0,
+                                    minCountSpot = 0)
+
+## deconvolution using CARDfree
+CARDfree_obj = CARD_refFree(CARDfree_obj)
+print(CARDfree_obj@Proportion_CARD[1:2,])
